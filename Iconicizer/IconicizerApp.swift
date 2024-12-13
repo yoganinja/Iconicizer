5726395e39605e1a28c27022bbddaff68797cb2b
John Florian <developer@johnflorian.com>
2024-12-13 06:52:01 -0800

    Initial Commit

---
+++ Iconicizer/IconicizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconicizerApp.swift
+//  Iconicizer
+//
+//  Created by John Florian on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct IconicizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IconicizerTests/IconicizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  IconicizerTests.swift
+//  IconicizerTests
+//
+//  Created by John Florian on 12/13/24.
+//
+
+import Testing
+@testable import Iconicizer
+
+struct IconicizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
